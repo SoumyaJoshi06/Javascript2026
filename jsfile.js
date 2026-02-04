0aa4c5257ac39053d2535df0071942262401a30c
SoumyaJoshi06 <soumyajoshi0506@gmail.com>
2026-02-04 15:55:08 +0530

    Commiting

--- jsfile.js
+++ jsfile.js
@@ -1,7 +1,41 @@
-console.log("HelloWorld")
-console.error("Not good job")
-console.info("ab")
-var a=20;
-let a=20;
+console.log("HelloWorld");
+console.error("Not good job");
+console.info("ab");
 const a=20; 
 console.log(a);
+let bool=true;
+let und;
+let nul=null;
+console.log(typeof bool);
+let x=7;
+let y=10;
+console.log(x+y);
+let age=45;
+if(age>=18){
+    console.log("yes");
+}else{
+    console.log("No");
+}
+
+let students={
+    name:"Varun",
+    age:27,
+}
+//function
+function add(p,q){
+    return p+q;
+}console.log(add(4,5));
+const multpiply=(p,q)=>p*q;
+console.log("arrow function",multpiply(5,10));
+console.log(Math.sqrt(100));
+//string
+txt="hello world";
+console.log(txt.toUpperCase());
+console.log(txt.includes("hel"));
+//type casting
+let str="1234";
+let convnum=Number(str);
+console.log(typeof convnum);
+console.log(str+10);
+console.log(convnum+10);
+
